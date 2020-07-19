0ca2216378e6fd447a04c889addaa0cc8de015f3
Alex Coulter <ALEXCOULTER.UNC@GMAIL.COM>
2020-07-18 21:39:17 -0400

    added spinning disc img

--- discgolfpro/src/App.js
+++ discgolfpro/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './disc.png';
+import disc from './aviar.png';
 import './App.css';
 
 function App() {
@@ -21,7 +22,8 @@ function App() {
          For all your Disc Golf needs
         </p>
       </header>
-      <span onClick={startRound}>Start a round -</span><span onClick={savedRound}>- View a saved round</span>
+      <span onClick={startRound}>Start a round -</span><span onClick={savedRound}>- View a saved round</span><br></br>
+      <img src={disc} className="disc" alt="spinning disc" />
     </div>
   );
 }
