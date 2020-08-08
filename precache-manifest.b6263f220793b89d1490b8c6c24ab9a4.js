0e7ba13671fbdeafa347d33a9b7441d79d4f308e
Alex Coulter <ALEXCOULTER.UNC@GMAIL.COM>
2020-08-07 22:35:32 -0400

    Updates

---
+++ precache-manifest.b6263f220793b89d1490b8c6c24ab9a4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f49a7ceee1ee5c026342636a5ccad7a",
+    "url": "/DiscGolfPro/index.html"
+  },
+  {
+    "revision": "acac0b9b193c923ab267",
+    "url": "/DiscGolfPro/static/css/main.7c06c8ba.chunk.css"
+  },
+  {
+    "revision": "a1e689b84e6e53a41bb6",
+    "url": "/DiscGolfPro/static/js/2.252d67e8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/DiscGolfPro/static/js/2.252d67e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acac0b9b193c923ab267",
+    "url": "/DiscGolfPro/static/js/main.58539c4f.chunk.js"
+  },
+  {
+    "revision": "fa058f239c18a13aafba",
+    "url": "/DiscGolfPro/static/js/runtime-main.ed09c8a7.js"
+  },
+  {
+    "revision": "b178ea08ddc72c6696bd05cb1590e2a8",
+    "url": "/DiscGolfPro/static/media/aviar.b178ea08.png"
+  },
+  {
+    "revision": "77668db093fa51f36c2f7c0ddaaa01c4",
+    "url": "/DiscGolfPro/static/media/bird.77668db0.gif"
+  },
+  {
+    "revision": "7e79118f42743b1699d534ea26eba3ff",
+    "url": "/DiscGolfPro/static/media/bobby.7e79118f.jpg"
+  },
+  {
+    "revision": "07a259fbb20564c3afe6a12725d9c199",
+    "url": "/DiscGolfPro/static/media/disc.07a259fb.png"
+  },
+  {
+    "revision": "04d0c5f9ffdf5b81214b29fc58b2317e",
+    "url": "/DiscGolfPro/static/media/milhouse.04d0c5f9.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/discgolfpro/precache-manifest.bafce405347bcb8ed7599dba359bd02a.js"
+  "/DiscGolfPro/precache-manifest.b6263f220793b89d1490b8c6c24ab9a4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/discgolfpro/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DiscGolfPro/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
